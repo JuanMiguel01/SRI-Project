b0f6a0b9a73d606447594a112d22306258035bc4
amandanoris <amandanoris34@gmail.com>
2024-03-03 15:46:07 -0500

    base visual

--- gui/app.py
+++ gui/app.py
@@ -1,1 +1,16 @@
-rgerg
+import streamlit as st
+from emoji import emojize
+
+st.title('Sistema de Recuperación de Información')
+
+col1, col2 = st.columns([4, 1])
+
+query = col1.text_input("Introduce tu consulta aquí", key="query_input")
+
+if query:
+    st.session_state['saved_query'] = query
+
+# Crear el botón de búsqueda con un emoji en la segunda columna
+if col2.button(emojize(":mag_right: Buscar")):
+    # Lógica de búsqueda aquí
+    st.write('Has hecho clic en el botón!')
